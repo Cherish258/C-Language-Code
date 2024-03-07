7384c0f8918080cc1f1f325785dc755c8b896995
Cherish258 <162443112+Cherish258@users.noreply.github.com>
2024-03-07 21:17:34 +0800

    Variable

---
+++ Variable.cpp
@@ -0,0 +1,16 @@
+// Variable
+
+#include <stdio.h>
+
+int main()
+{
+	int year = 2024;
+	double height = 177.7;
+
+	year = year + 1;
+	height = height - 1.535;
+
+	printf("%d\n", year);
+	printf("%lf\n", height);
+	return 0;
+}
