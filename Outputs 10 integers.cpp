f422163284a5b881d2f96f8c897fb28bd2e57631
Cherish258 <162443112+Cherish258@users.noreply.github.com>
2024-03-08 22:24:32 +0800

    Outputs 10 integers

---
+++ Outputs 10 integers.cpp
@@ -0,0 +1,16 @@
+// Outputs 10 integers
+#include <stdio.h>
+
+int main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int i = 0;
+	
+	//Output
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
