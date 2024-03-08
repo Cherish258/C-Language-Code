3953dc355e87dcccb120296550321ab47980ceb1
Cherish258 <162443112+Cherish258@users.noreply.github.com>
2024-03-08 22:29:02 +0800

    Update Outputs 10 integers.cpp

--- Outputs 10 integers.cpp
+++ Outputs 10 integers.cpp
@@ -4,12 +4,14 @@
 int main()
 {
 	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int *p = &arr[0];
 	int i = 0;
 	
 	//Output
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d\n", arr[i]);
+		printf("%d\n", *p);
+		p++;
 	}
 
 	return 0;
