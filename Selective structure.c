68faa356b263c718e70ecd75b37ddeadf51e65b6
Cherish258 <162443112+Cherish258@users.noreply.github.com>
2024-03-10 12:31:19 +0800

    Selective structure

---
+++ Selective structure.c
@@ -0,0 +1,17 @@
+// Selective structure
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char input = 'Y';
+
+	printf("Will World War III break out?(Y/N)\n");
+	scanf("%c", &input);
+	if (input == 'Y')
+		printf("You're right!");
+	else
+		printf("You're wrong!");
+
+	return 0;
+}
