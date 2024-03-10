df9a0e067d7c26841ff2664636e63875019e8627
Cherish258 <162443112+Cherish258@users.noreply.github.com>
2024-03-10 10:02:54 +0800

    Update Array of strings.c

--- Array of strings.c
+++ Array of strings.c
@@ -1,13 +1,16 @@
 // Array of strings
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	char arr1[] = "abc";
 	char arr2[] = { 'a', 'b', 'c', '\0' }; // \0 -- End identifier
+	int len = strlen("abc"); // Find the length of the string
 
 	printf("%s\n", arr1);
 	printf("%s\n", arr2);
+	printf("%d\n", len);
 
 	return 0;
 }
